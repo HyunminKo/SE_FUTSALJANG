2558f32ee6a16a615cd93df51042d1be5d73c867
unknown <>
2016-12-04 15:02:36 +0900

    페이지일정하게 보이게수정했음fixed

--- assets/js/reservationpay.js
+++ assets/js/reservationpay.js
@@ -2,13 +2,11 @@ function div_show(s){
 	if(s == "cardit"){
 		document.getElementById("choice_card1").style.display = "";
 		document.getElementById("choice_card2").style.display = "";
-		document.getElementById("choice_card3").style.display = "";
 		document.getElementById("choice_online1").style.display = "none";
 		document.getElementById("choice_online2").style.display = "none";
 	}else{
 		document.getElementById("choice_card1").style.display = "none";
 		document.getElementById("choice_card2").style.display = "none";
-		document.getElementById("choice_card3").style.display = "none";
 		document.getElementById("choice_online1").style.display = "";
 		document.getElementById("choice_online2").style.display = "";
 	}
